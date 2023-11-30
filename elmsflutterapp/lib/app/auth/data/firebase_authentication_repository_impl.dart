ad068af75bf02fc9a9e56c819b1a7c03b35d8f51
Revanth Reddy Maturu <mrevanth1999@gmail.com>
2023-11-29 23:35:26 -0500

    Comments for code components

--- elmsflutterapp/lib/app/auth/data/firebase_authentication_repository_impl.dart
+++ elmsflutterapp/lib/app/auth/data/firebase_authentication_repository_impl.dart
@@ -1,3 +1,4 @@
+// Import statements for the required packages and classes
 import 'package:elmsflutterapp/app/auth/data/user_config.dart';
 import 'package:elmsflutterapp/app/auth/domain/usecases/authenticate_with_email_password_usecase.dart';
 import 'package:elmsflutterapp/app/auth/domain/usecases/forgot_password_usecase.dart';
@@ -6,13 +7,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import '../domain/repository/authentication_repository.dart';
 
+// Class definition for the FirebaseAuthenticationRepository implementing AuthenticationRepository
 class FirebaseAuthenticationRepository implements AuthenticationRepository {
+  // Constants for different error codes related to authentication
   final String errorCodeWrongPassword = "wrong-password";
   final String errorCodeUserNotFound = "user-not-found";
   final String errorCodeInvalidEmail = "invalid-email";
   final String errorCodeNetworkRequestFailed = "network-request-failed";
   final String errorCodeTooManyRequest = "too-many-requests";
 
+  // Method to check the login status
   @override
   Future<bool> checkLoginStatus() async {
     User? firebaseUser = FirebaseAuth.instance.currentUser;
@@ -22,8 +26,8 @@ class FirebaseAuthenticationRepository implements AuthenticationRepository {
       if (isUserAnInstructor == null) {
         return false;
       }
+      // Resetting user data and setting user configuration
       UserConfig.resetUserData();
-
       UserConfig(
           uid: FirebaseAuth.instance.currentUser!.uid,
           email: FirebaseAuth.instance.currentUser!.email!,
@@ -35,6 +39,7 @@ class FirebaseAuthenticationRepository implements AuthenticationRepository {
     }
   }
 
+  // Method to authenticate with email and password
   @override
   Future<String> authenticateWithEmailAndPassword(
       {required String email,
@@ -49,6 +54,7 @@ class FirebaseAuthenticationRepository implements AuthenticationRepository {
       SharedPreferences prefs = await SharedPreferences.getInstance();
       prefs.setInt('isUserAnInstructor', isUserAnInstructor ? 1 : 0);
 
+      // Resetting user data and setting user configuration
       UserConfig.resetUserData();
       UserConfig(
           uid: FirebaseAuth.instance.currentUser!.uid,
@@ -57,11 +63,12 @@ class FirebaseAuthenticationRepository implements AuthenticationRepository {
 
       return FirebaseAuth.instance.currentUser!.uid;
     } on FirebaseAuthException catch (error) {
-      FirebaseAuthException firebaseAuthxception = error;
-      String errorCode = firebaseAuthxception.code;
-      String? errorMessage = firebaseAuthxception.message;
+      FirebaseAuthException firebaseAuthException = error;
+      String errorCode = firebaseAuthException.code;
+      String? errorMessage = firebaseAuthException.message;
       print('ERROR_CODE:$errorCode');
       print('ERROR_MESSAGE:$errorMessage');
+      // Handling specific authentication errors and throwing custom exceptions
       if (errorCode == errorCodeWrongPassword ||
           errorCode == errorCodeUserNotFound ||
           errorCode == errorCodeInvalidEmail) {
@@ -79,16 +86,18 @@ class FirebaseAuthenticationRepository implements AuthenticationRepository {
     }
   }
 
+  // Method to initiate the forgot password process
   @override
   Future<void> forgotPassword({required String emailId}) async {
     try {
       await FirebaseAuth.instance.sendPasswordResetEmail(email: emailId);
     } on FirebaseAuthException catch (error) {
-      FirebaseAuthException firebaseAuthxception = error;
-      String errorCode = firebaseAuthxception.code;
-      String? errorMessage = firebaseAuthxception.message;
+      FirebaseAuthException firebaseAuthException = error;
+      String errorCode = firebaseAuthException.code;
+      String? errorMessage = firebaseAuthException.message;
       print('ERROR_CODE:$errorCode');
       print('ERROR_MESSAGE:$errorMessage');
+      // Handling specific errors related to forgot password and throwing custom exceptions
       if (errorCode == errorCodeUserNotFound ||
           errorCode == errorCodeInvalidEmail) {
         throw ForgotPasswordInvalidEmailPasswordException();
@@ -104,12 +113,14 @@ class FirebaseAuthenticationRepository implements AuthenticationRepository {
     }
   }
 
+  // Method to log out the user
   @override
   Future<void> logout() async {
     await FirebaseAuth.instance.signOut();
     SharedPreferences prefs = await SharedPreferences.getInstance();
 
     prefs.remove('isUserAnInstructor');
+    // Resetting user data
     UserConfig.resetUserData();
     return;
   }
--- elmsflutterapp/lib/app/auth/domain/usecases/authenticate_with_email_password_usecase.dart
+++ elmsflutterapp/lib/app/auth/domain/usecases/authenticate_with_email_password_usecase.dart
@@ -2,40 +2,54 @@ import 'dart:async';
 import 'package:elmsflutterapp/app/auth/domain/repository/authentication_repository.dart';
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
 
+// Use case class for authenticating a user with email and password
 class AuthenticateWithEmailAndPasswordUseCase
     extends CompletableUseCase<AuthenticateWithEmailAndPasswordParams> {
+  // Repository responsible for handling authentication-related operations
   final AuthenticationRepository _repository;
 
+  // Constructor that takes an authentication repository as a dependency
   AuthenticateWithEmailAndPasswordUseCase(this._repository);
 
+  // Override method to build the use case stream
   @override
   Future<Stream<String>> buildUseCaseStream(
       AuthenticateWithEmailAndPasswordParams? params) async {
+    // Create a stream controller to handle the asynchronous response
     final StreamController<String> streamController = StreamController();
     try {
+      // Attempt to authenticate the user using the repository
       String userId = await _repository.authenticateWithEmailAndPassword(
           email: params!._email, 
           password: params._password,
           isUserAnInstructor: params.isUserAnInstructor);
+      
+      // Print a success message and add the user ID to the stream
       print("Authentication successful");
       streamController.add(userId);
       streamController.close();
     } catch (error) {
+      // If an error occurs during authentication, add the error to the stream
       streamController.addError(error);
       print("Authentication unsuccessful");
     }
+    // Return the stream for observing the authentication process
     return streamController.stream;
   }
 }
 
+// Parameters class for the authenticateWithEmailAndPassword use case
 class AuthenticateWithEmailAndPasswordParams {
+  // Private properties for email, password, and user type
   String _email;
   String _password;
   bool isUserAnInstructor;
 
+  // Constructor to initialize the parameters
   AuthenticateWithEmailAndPasswordParams(this._email, this._password, this.isUserAnInstructor);
 }
 
+// Custom exceptions for handling different authentication error scenarios
 class LoginInvalidEmailPasswordException implements Exception {}
 
 class LoginNetworkRequestFailedException implements Exception {}

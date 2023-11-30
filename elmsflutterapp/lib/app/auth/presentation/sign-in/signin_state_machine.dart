2f5c40c57b74f41f32d6086d153972a5469f6465
Revanth Reddy Maturu <mrevanth1999@gmail.com>
2023-11-30 00:40:56 -0500

    Code refactor and quality review+comments

--- elmsflutterapp/lib/app/auth/presentation/sign-in/signin_state_machine.dart
+++ elmsflutterapp/lib/app/auth/presentation/sign-in/signin_state_machine.dart
@@ -1,12 +1,18 @@
+// Import necessary packages and files
 import '../../../../core/presentation/state_machine.dart';
 
+// Define the SignInStateMachine class, extending from StateMachine
 class SignInStateMachine extends StateMachine<SignInState?, SignInEvent> {
+  // Constructor initializing the state machine with SignInInitState
   SignInStateMachine() : super(new SignInInitState());
 
+  // Override method to determine the next state based on the event
   @override
   SignInState? getStateOnEvent(SignInEvent event) {
     final eventType = event.runtimeType;
     SignInState? newState = getCurrentState();
+
+    // Switch statement to handle different event types
     switch (eventType) {
       case SignInInitEvent:
         newState = SignInInitState();
@@ -32,20 +38,19 @@ class SignInStateMachine extends StateMachine<SignInState?, SignInEvent> {
   }
 }
 
+// Define abstract class SignInEvent
 abstract class SignInEvent {}
 
+// Define specific events that extend SignInEvent
 class SignInInitEvent extends SignInEvent {}
-
 class SignInClickedEvent extends SignInEvent {}
-
 class ForgotPasswordClickedEvent extends SignInEvent {}
-
 class PasswordResetRequestedEvent extends SignInEvent {}
-
 class SignInErrorEvent extends SignInEvent {}
 
+// Define abstract class SignInState
 abstract class SignInState {}
 
+// Define specific states that extend SignInState
 class SignInInitState extends SignInState {}
-
 class SignInLoadingState extends SignInState {}
